d5176107c1889366c55b5bb53ceb5e2b4bfdb871
rose-rawal <rooserwl@gmail.com>
2024-07-27 08:07:24 +0545

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MultiProj.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
