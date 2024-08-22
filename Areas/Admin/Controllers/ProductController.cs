c70d701a300a1496ef723a41338f656f51e8ab39
rose-rawal <rooserwl@gmail.com>
2024-08-23 00:37:49 +0545

    Product  crud reposiory created and integrated wih unit of work and controller created for product get all done other remaining

---
+++ Areas/Admin/Controllers/ProductController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using multiproj.DataAccess.Repository;
+using multiproj.DataAccess.Repository.IRepository;
+
+namespace MultiProj.Areas.Admin.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public ProductController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        
+        public IActionResult Index()
+        {
+            var allProducts=_unitOfWork.Product.GetAll().ToList();
+            return View(allProducts);
+        }
+    }
+}
