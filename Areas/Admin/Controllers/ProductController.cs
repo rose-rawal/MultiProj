343e1e5c1173931acc029086415ccf43eb41e480
rose-rawal <rooserwl@gmail.com>
2024-08-25 18:23:33 +0545

    self edit and create of product done

--- Areas/Admin/Controllers/ProductController.cs
+++ Areas/Admin/Controllers/ProductController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using multiproj.DataAccess.Repository;
 using multiproj.DataAccess.Repository.IRepository;
+using multiproj.Models.Models;
 
 namespace MultiProj.Areas.Admin.Controllers
 {
@@ -11,11 +12,45 @@ namespace MultiProj.Areas.Admin.Controllers
         {
             _unitOfWork = unitOfWork;
         }
-        
+        [HttpGet]
         public IActionResult Index()
         {
             var allProducts=_unitOfWork.Product.GetAll().ToList();
             return View(allProducts);
+        }
+        [HttpGet]
+        public IActionResult CreateProduct(int id) {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult CreateProduct(Product product)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(product);
+            }
+            _unitOfWork.Product.Add(product);
+            _unitOfWork.Save();
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult EditProduct(int id)
+        {
+            Product product = _unitOfWork.Product.GetValue(pr=>pr.ProductId==id);
+            return View(product);
+        }
+
+        [HttpPost]
+        public IActionResult EditProduct(Product product)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(product);
+            }
+            _unitOfWork.Product.Update(product);
+            _unitOfWork.Save();
+            return RedirectToAction("Index");
         }
     }
 }
